3db1af356e62ecfdc24e7808c43332af0561b99e
Sujal Singh <email.sujalsingh@gmail.com>
2024-02-14 09:15:08 +0530

    web: init wireguard panel

---
+++ web/src/components/Panels/WireGuardPanel.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import Typography from "@mui/material/Typography";
+
+import PanelBase from "./PanelBase.jsx";
+
+
+function Dashboard() {
+    return (
+        <PanelBase title={"WireGuard Config"}>
+            <Typography variant="body2">Nothing here yet.</Typography>
+        </PanelBase>
+    );
+}
+
+
+export default Dashboard;
