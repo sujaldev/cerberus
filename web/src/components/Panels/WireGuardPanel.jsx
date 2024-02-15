631aae5711d5971f30cecb762e0fcd86dd9412b8
Sujal Singh <email.sujalsingh@gmail.com>
2024-02-15 21:34:11 +0530

    web: loading animation in wireguard panel

--- web/src/components/Panels/WireGuardPanel.jsx
+++ web/src/components/Panels/WireGuardPanel.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import QRCode from "react-qr-code";
 import Paper from "@mui/material/Paper";
-import Typography from "@mui/material/Typography";
+import Skeleton from "@mui/material/Skeleton";
 import SyntaxHighlighter from "react-syntax-highlighter";
 import {androidstudio} from "react-syntax-highlighter/dist/esm/styles/hljs";
 
@@ -11,28 +11,37 @@ import PanelBase from "./PanelBase.jsx";
 function WireGuardPanel({conf}) {
     return (
         <PanelBase title="WireGuard Config">
-            {conf ?
-                <div style={{display: "flex", alignItems: "center", justifyContent: "space-evenly"}}>
-                    <div>
-                        <Paper variant="outlined" style={{
-                            display: "inline-flex",
-                            background: "white",
-                            padding: "10px"
-                        }}>
-                            <QRCode size={180} value={conf}/>
-                        </Paper>
-                    </div>
+            <div style={{display: "flex", alignItems: "center", justifyContent: "space-evenly"}}>
+                {conf ?
+                    <>
+                        <div>
+                            <Paper variant="outlined" style={{
+                                display: "inline-flex",
+                                background: "white",
+                                padding: "10px"
+                            }}>
+                                <QRCode size={180} value={conf}/>
+                            </Paper>
+                        </div>
 
-                    <SyntaxHighlighter language="ini" style={androidstudio} customStyle={{
-                        fontSize: "12px",
-                        padding: "20px"
-                    }}>
-                        {conf}
-                    </SyntaxHighlighter>
-                </div>
-                :
-                <Typography variant="body1">Empty config</Typography>
-            }
+                        <SyntaxHighlighter language="ini" style={androidstudio} customStyle={{
+                            fontSize: "12px",
+                            padding: "20px"
+                        }}>
+                            {conf}
+                        </SyntaxHighlighter>
+                    </>
+                    :
+                    <>
+                        <Skeleton width="60%" height="200px" style={{borderRadius: "5px"}} variant="rectangular"/>
+                        <Skeleton style={{
+                            width: "28%",
+                            paddingBottom: "28%",
+                            borderRadius: "5px",
+                        }} variant="rectangular"/>
+                    </>
+                }
+            </div>
         </PanelBase>
     );
 }
