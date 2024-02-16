3af7f3a068e7e29e55b1746f490c4b1c93fe6519
Sujal Singh <email.sujalsingh@gmail.com>
2024-02-16 21:03:38 +0530

    web: use react router

--- web/src/main.jsx
+++ web/src/main.jsx
@@ -1,16 +1,32 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import {
+    createBrowserRouter,
+    RouterProvider,
+} from "react-router-dom";
 import App from "./pages/App.jsx";
+import ErrorPage from "./pages/ErrorPage.jsx";
 
 import CssBaseline from "@mui/material/CssBaseline";
 import {ThemeProvider} from "@mui/material/styles";
 import theme from "./theme";
 
+const router = createBrowserRouter([
+    {
+        "path": "/",
+        "element": <App/>
+    },
+    {
+        "path": "/block",
+        "element": <ErrorPage/>
+    }
+]);
+
 ReactDOM.createRoot(document.getElementById("root")).render(
     <React.StrictMode>
         <ThemeProvider theme={theme}>
             <CssBaseline></CssBaseline>
-            <App/>
+            <RouterProvider router={router}/>
         </ThemeProvider>
     </React.StrictMode>,
 );
