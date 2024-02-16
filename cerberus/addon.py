6a1a0d727d6fb6e1d75500d2d5a57138ae83c0ba
Sujal Singh <email.sujalsingh@gmail.com>
2024-02-16 13:29:45 +0530

    core: implemented wireguard config endpoint

--- cerberus/addon.py
+++ cerberus/addon.py
@@ -1,16 +1,17 @@
 import textwrap
 
-from mitmproxy import http
 from mitmproxy import ctx
-from mitmproxy.net.local_ip import get_local_ip, get_local_ip6
+from mitmproxy import http
 from mitmproxy_rs import pubkey
+from mitmproxy.net.local_ip import get_local_ip, get_local_ip6
 
 
 class Addon:
     def __init__(self):
         self.wg_conf = self.get_wireguard_config()
 
-    def get_wireguard_config(self):
+    @staticmethod
+    def get_wireguard_config() -> str:
         # There has to be a better way than this.
         host = get_local_ip() or get_local_ip6()
         port = ctx.options.listen_port
@@ -29,8 +30,16 @@ class Addon:
             """
         ).strip()
 
+    def middleware_recv(self, flow: http.HTTPFlow) -> None:
+        # Handles receiving config data from middleware.
+        path_components = flow.request.path_components[1:]
+
+        if path_components[0] == "wireguard":
+            flow.response = http.Response.make(content=self.wg_conf.encode())
+
     def request(self, flow: http.HTTPFlow) -> None:
-        print(flow.request)
+        if flow.request.host_header == "mitm.it" and flow.request.path_components[0] == "cerberus":
+            return self.middleware_recv(flow)
 
 
 addons = [Addon()]
