092e65cf818ed3314ef2663a403e52f434b71dc7
Sujal Singh <email.sujalsingh@gmail.com>
2024-02-18 09:48:35 +0530

    core: ad hoc geoblocking
    this was implemented just before the presentation :)

--- cerberus/addon.py
+++ cerberus/addon.py
@@ -1,7 +1,12 @@
+import socket
+from pathlib import Path
+
 from typo import SpellChecker
 from middleware_handler import MiddlewareHandler
 from helpers import Router
 
+import geoip2.database
+import geoip2.errors
 from mitmproxy import http
 
 
@@ -9,6 +14,10 @@ class Addon(Router):
     def __init__(self):
         self.spellchecker = SpellChecker()
         self.middleware_handler = MiddlewareHandler()
+
+        self.geoip_reader = geoip2.database.Reader(
+            (Path(__file__).parent / "geo.mmdb").resolve()
+        )
 
         super().__init__(Router.combine_namespaces([
             self.spellchecker,
@@ -27,8 +36,24 @@ class Addon(Router):
 
     def perform_checks(self, flow: http.HTTPFlow) -> str:
         # Returns a string describing which check matched the given flow
-        if self.spellchecker.process_typo(flow):
+        if self.geoblock(flow):
+            return "geoblocked"
+        elif self.spellchecker.process_typo(flow):
             return "typo"
+
+    def geoblock(self, flow: http.HTTPFlow) -> bool:
+        try:
+            ip = socket.gethostbyname(flow.request.host_header)
+            if self.geoip_reader.city(ip).country.iso_code in ("RU", "PK", "CN"):
+                flow.response = http.Response.make(
+                    403,
+                    content=b"<h1>This website is hosted in a country that is blacklisted</h1>"
+                )
+                return True
+        except socket.gaierror:
+            return False
+        except geoip2.errors.AddressNotFoundError:
+            return False
 
 
 addons = [Addon()]
